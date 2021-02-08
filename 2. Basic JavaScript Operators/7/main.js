257e2806c19a23ca07554e5deeb11fd93f300c85
Pavel <vesna-78d@mail.ru>
2021-02-08 13:50:35 +0300

    completed task 7

---
+++ 2. Basic JavaScript Operators/7/main.js
@@ -0,0 +1,14 @@
+let a = null > 0;
+alert("null > 0 " + a);
+
+a = null < 0;
+alert("null < 0 " + a);
+
+a = null == 0;
+alert("null == 0 " + a);
+
+a = null >= 0;
+alert("null >= 0 " + a);
+
+a = null <= 0;
+alert("null <= 0 " + a);
